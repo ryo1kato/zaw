9348190f47b34dcfda568c257b78b14e309b3207
Ryoichi KATO <ryo1kato@gmail.com>
2015-03-17 15:06:09 +0900

    history: never execute, only replace buffer.

--- sources/history.zsh
+++ sources/history.zsh
@@ -2,8 +2,10 @@ zmodload zsh/parameter
 
 function zaw-src-history() {
     cands_assoc=("${(@kv)history}")
-    actions=("zaw-callback-execute" "zaw-callback-replace-buffer" "zaw-callback-append-to-buffer")
-    act_descriptions=("execute" "replace edit buffer" "append to edit buffer")
+    #actions=("zaw-callback-execute" "zaw-callback-replace-buffer" "zaw-callback-append-to-buffer")
+    #act_descriptions=("execute" "replace edit buffer" "append to edit buffer")
+    actions=("zaw-callback-append-to-buffer")
+    act_descriptions=("append to edit buffer")
     options=("-r" "-m")
 
     if (( $+functions[zaw-bookmark-add] )); then
